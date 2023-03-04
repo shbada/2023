bd7a4f891f9912f7a1b9b303b36605b60a979901
seohaem <seohae0406@gmail.com>
2023-03-04 23:21:17 +0900

    (03.04) 완료

--- toby_springboot/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ toby_springboot/helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -3,10 +3,15 @@ package tobyspring.helloboot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @RestController
 public class HelloController {
     @GetMapping("/hello")
     public String hello(String name) {
-        return "Hello " + name;
+        SimpleHelloService simpleHelloService = new SimpleHelloService();
+
+        // null 이라면 예외를 던진다.
+        return simpleHelloService.sayHello(Objects.requireNonNull(name));
     }
 }
