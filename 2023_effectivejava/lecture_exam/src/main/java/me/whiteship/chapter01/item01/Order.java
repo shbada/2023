549e6e205d697b28b1dcf6e420dd4580e2ad31ba
seohaem <seohae0406@gmail.com>
2023-03-31 21:40:36 +0900

    (03.31) 강의

--- 2023_effectivejava/lecture_exam/src/main/java/me/whiteship/chapter01/item01/Order.java
+++ 2023_effectivejava/lecture_exam/src/main/java/me/whiteship/chapter01/item01/Order.java
@@ -12,6 +12,34 @@ public class Order {
 
     private OrderStatus orderStatus;
 
+//    public Order(boolean prime, boolean urgent, Product product, OrderStatus orderStatus) {
+//        this.prime = prime;
+//        this.urgent = urgent;
+//        this.product = product;
+//        this.orderStatus = orderStatus;
+//    }
+
+    /**
+     * 아래 두 생성자는 파라미터 타입이 동일한 prime, urgent 때문에 선언할 수 없다.
+     */
+//    public Order(boolean prime, Product product, OrderStatus orderStatus) {
+//        this.prime = prime;
+//        this.urgent = urgent;
+//        this.product = product;
+//        this.orderStatus = orderStatus;
+//    }
+//
+//    public Order(boolean urgent, Product product, OrderStatus orderStatus) {
+//        this.prime = prime;
+//        this.urgent = urgent;
+//        this.product = product;
+//        this.orderStatus = orderStatus;
+//    }
+
+    /**
+     * 생성자 -> 정적팩토리 메서드
+     * 1. 메서드 명을 지정할 수 있다. (객체의 특징을 좀더 분명하게 표현할 수 있다.)
+     */
     public static Order primeOrder(Product product) {
         Order order = new Order();
         order.prime = true;
