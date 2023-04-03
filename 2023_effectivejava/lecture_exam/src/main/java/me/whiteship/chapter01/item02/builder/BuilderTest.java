8439b0a893b5fe64f000d4e5507ae027e0e0b5e9
seohaebada <seohae0406@gmail.com>
2023-04-03 22:37:14 +0900

    (04.03) 완료

--- 2023_effectivejava/lecture_exam/src/main/java/me/whiteship/chapter01/item02/builder/BuilderTest.java
+++ 2023_effectivejava/lecture_exam/src/main/java/me/whiteship/chapter01/item02/builder/BuilderTest.java
@@ -3,6 +3,9 @@ package me.whiteship.chapter01.item02.builder;
 public class BuilderTest {
 
     public static void main(String[] args) {
+        // 필수 속성을 설정할 수 있다. (객체를 안전하게, 반드시 받아오게끔 강제할 수 있음)
+        // 옵션 속성은 설정해도되고 안해도된다.
+        // 반드시 빌더패턴을 사용하지 말고, immutable 하게 만들고 싶을때 사용하자.
         new NutritionFacts.Builder(10, 10)
                 .calories(10)
                 .build();
--- 2023_effectivejava/lecture_exam/src/main/java/me/whiteship/chapter01/item02/builder/NutritionFacts.java
+++ 2023_effectivejava/lecture_exam/src/main/java/me/whiteship/chapter01/item02/builder/NutritionFacts.java
@@ -1,6 +1,14 @@
 package me.whiteship.chapter01.item02.builder;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
 // 코드 2-3 빌더 패턴 - 점층적 생성자 패턴과 자바빈즈 패턴의 장점만 취했다. (17~18쪽)
+// @Builder // lombok annotation 사용 가능
+// 위 어노테이션이 있으면, 컴파일 시점에 이 클래스 코드를 lombok이 조작한다.
+// 롬복 사용의 단점 : 필수 파라미터를 지정해줄 수 없다. -> 이 경우에는 직접 작성하자.
+//@AllArgsConstructor(access = AccessLevel.PRIVATE) // 내부에서 빌더에서만 사용가능함
 public class NutritionFacts {
     private final int servingSize;
     private final int servings;
@@ -32,12 +40,16 @@ public class NutritionFacts {
             this.servings    = servings;
         }
 
+        /* Builder 타입을 리턴 */
         public Builder calories(int val)
         { calories = val;      return this; }
+
         public Builder fat(int val)
         { fat = val;           return this; }
+
         public Builder sodium(int val)
         { sodium = val;        return this; }
+
         public Builder carbohydrate(int val)
         { carbohydrate = val;  return this; }
 
