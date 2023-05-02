38612aa5de1c31d3d1ecf9b1e886cb08d2c59923
서해바다 <87924260+seohaebada@users.noreply.github.com>
2023-05-02 23:10:54 +0900

    Delete budget-side-project directory

--- budget-side-project/memeber-api/src/main/kotlin/com/member/api/infrastrucrue/MemberRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.member.api.infrastrucrue
-
-import com.member.api.domain.Member
-import org.springframework.data.mongodb.repository.MongoRepository
-
-interface MemberRepository : MongoRepository<Member?, String?>
