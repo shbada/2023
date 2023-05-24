747d8752cf7e4d6cb014181fd2585c5dc6356a6e
seohaebada <seohae0406@gmail.com>
2023-05-24 19:13:08 +0900

    (05.24) crud

---
+++ mongodb_query/query/CRUD.sql
@@ -0,0 +1,9 @@
+-- insert
+db.customers.insertOne({ _id : "bob@gmail.com", name: "Robert Smit
+h", orders: [], spend: 0, lastpurchase: null })
+
+db.customers.insertOne({ _id : "bob@gmail.com", name: "Bobby Smith
+", orders: [], spend: 0, lastpurchase: null })
+
+db.customers.insertOne({name: "Andi Smith", orders: [], spend: 0,
+    lastpurchase: null })
