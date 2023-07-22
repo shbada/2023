2d01c5ca0f767044c918f30c8dfb2d01c2073303
seohaebada <seohae0406@gmail.com>
2023-07-22 11:14:38 +0900

    (07.22) item17

--- 2023_effectivejava_2/effective-java-part2/src/main/java/me/whiteship/chapter04/item17/part1/Person.java
+++ 2023_effectivejava_2/effective-java-part2/src/main/java/me/whiteship/chapter04/item17/part1/Person.java
@@ -2,6 +2,9 @@ package me.whiteship.chapter04.item17.part1;
 
 public final class Person {
 
+    /**
+     * 자신 외에는 내부의 가변 컴포넌트에 접근할 수 없도록 한다.
+     */
     private final Address address;
 
     public Person(Address address) {
@@ -9,11 +12,15 @@ public final class Person {
     }
 
     public Address getAddress() {
+        // 복사본을 만든다.
         Address copyOfAddress = new Address();
         copyOfAddress.setStreet(address.getStreet());
         copyOfAddress.setZipCode(address.getZipCode());
         copyOfAddress.setCity(address.getCity());
         return copyOfAddress;
+
+        // 이렇게하면 set 메서드로 값 변경이 가능해져버린다. (address 내부 필드)
+//        return address;
     }
 
     public static void main(String[] args) {
--- 2023_effectivejava_2/effective-java-part2/src/main/java/me/whiteship/chapter04/item17/part1/PhoneNumber.java
+++ 2023_effectivejava_2/effective-java-part2/src/main/java/me/whiteship/chapter04/item17/part1/PhoneNumber.java
@@ -1,7 +1,17 @@
 package me.whiteship.chapter04.item17.part1;
 
+/**
+ * 상속불가
+ * final class
+ * private 생성자만 가지고있게
+ */
 public final class PhoneNumber {
 
+    /**
+     * 불면클래스로 개발하는것이 훨씬 코딩하는데 쉽고 안전하다.
+     *
+     * final로 하지않으면 실수로라도 set메서드나 별도 메서드에서 값을 바꾸는 경우가 생길 수 있다.
+     */
     private final short areaCode, prefix, lineNum;
 
     public PhoneNumber(short areaCode, short prefix, short lineNum) {
